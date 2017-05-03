3d82aade526aaa7c12ae7dcabc11880eb3cc8f29
Guillaume VanderEst <guillaume@vanderest.org>
2017-05-03 14:10:17 -0700

    chore(ansi): clean up style of ansi color remap

--- utils/ansi.py
+++ utils/ansi.py
@@ -15,32 +15,47 @@ class Ansi(object):
     REAL_BRACKET_SYMBOL = '((bracket))'
 
     REMAPS = {
-        "{r": CSI + "0;31m", "{1": CSI + "0;31m",  # Red
-        "{R": CSI + "1;31m", "{!": CSI + "1;31m",  # Bright Red
-        "{g": CSI + "0;32m", "{2": CSI + "0;32m",  # Green
-        "{G": CSI + "1;32m", "{@": CSI + "1;32m",  # Bright Green
-        "{y": CSI + "0;33m", "{3": CSI + "0;33m",  # Yellow
-        "{Y": CSI + "1;33m", "{#": CSI + "1;33m",  # Bright Yellow
-        "{b": CSI + "0;34m", "{4": CSI + "0;34m",  # Blue
-        "{B": CSI + "1;34m", "{$": CSI + "1;34m",  # Bright Blue
-        "{m": CSI + "0;35m", "{5": CSI + "0;35m",  # Magenta
-        "{M": CSI + "1;35m", "{%": CSI + "1;35m",  # Bright Magenta
-        "{c": CSI + "0;36m", "{6": CSI + "0;36m",  # Cyan
-        "{C": CSI + "1;36m", "{^": CSI + "1;36m",  # Bright Cyan
-        "{w": CSI + "0;37m", "{7": CSI + "0;37m",  # White
-        "{W": CSI + "1;37m", "{&": CSI + "1;37m",  # Bright White
-        "{8": CSI + "1;30m", "{*": CSI + "1;30m", '{D': CSI + '1;30m',  # Grey
-        "{0": CSI + "0m", "{x": CSI + "0m", '{.': CSI + '0m',  # Reset
+        "{r": CSI + "0;31m",  # Red
+        "{1": CSI + "0;31m",  # Red
+        "{R": CSI + "1;31m",  # Bright Red
+        "{!": CSI + "1;31m",  # Bright Red
+        "{g": CSI + "0;32m",  # Green
+        "{2": CSI + "0;32m",  # Green
+        "{G": CSI + "1;32m",  # Bright Green
+        "{@": CSI + "1;32m",  # Bright Green
+        "{y": CSI + "0;33m",  # Yellow
+        "{3": CSI + "0;33m",  # Yellow
+        "{Y": CSI + "1;33m",  # Bright Yellow
+        "{#": CSI + "1;33m",  # Bright Yellow
+        "{b": CSI + "0;34m",  # Blue
+        "{4": CSI + "0;34m",  # Blue
+        "{B": CSI + "1;34m",  # Bright Blue
+        "{$": CSI + "1;34m",  # Bright Blue
+        "{m": CSI + "0;35m",  # Magenta
+        "{5": CSI + "0;35m",  # Magenta
+        "{M": CSI + "1;35m",  # Bright Magenta
+        "{%": CSI + "1;35m",  # Bright Magenta
+        "{c": CSI + "0;36m",  # Cyan
+        "{6": CSI + "0;36m",  # Cyan
+        "{C": CSI + "1;36m",  # Bright Cyan
+        "{^": CSI + "1;36m",  # Bright Cyan
+        "{w": CSI + "0;37m",  # White
+        "{7": CSI + "0;37m",  # White
+        "{W": CSI + "1;37m",  # Bright White
+        "{&": CSI + "1;37m",  # Bright White
+        "{8": CSI + "1;30m",  # Grey
+        "{*": CSI + "1;30m",  # Grey
+        '{D': CSI + '1;30m',  # Grey
+        "{0": CSI + "0m",  # Reset
+        "{x": CSI + "0m",  # Reset
+        '{.': CSI + '0m',  # Reset
 
         # Extended Color Reference:
         # http://www.calmar.ws/vim/256-xterm-24bit-rgb-color-chart.html
-        "{o": CSI + "38;5;130m" + CSI + "38;5;130m",  # Dark Orange
-        "{O": CSI + "38;5;202m" + CSI + "38;5;202m",  # Bright Orange
-        "{p": CSI + "38;5;55m" + CSI + "38;5;55m",  # Dark Purple
-        "{P": CSI + "38;5;129m" + CSI + "38;5;129m",  # Bright Purple
-
-        # Extra Special
-        "{H": CSI + "25l",  # Hide Cursor
+        "{o": CSI + "38;5;130m",  # Dark Orange
+        "{O": CSI + "38;5;202m",  # Bright Orange
+        "{p": CSI + "38;5;55m",  # Dark Purple
+        "{P": CSI + "38;5;129m",  # Bright Purple
     }
 
     @classmethod
