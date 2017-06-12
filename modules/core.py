149f2e9b1f665d05afdfe10d4a0f7c1b5a474a91
Guillaume VanderEst <guillaume@vanderest.org>
2017-06-12 14:31:04 -0700

    fix(combat): handle death in combat better for saving self/target

--- modules/core.py
+++ modules/core.py
@@ -1403,17 +1403,20 @@ class Actor(Object):
             target_room = target.get_room()
             if target.is_dead() or target_room != room:
                 target.save()
-
                 if not targets:
                     break
                 target = targets.pop(0)
 
             amount = random.randint(100, 300)
+
             self.echo("{BYour punch {r*{R*{r* {bDE{BV{wASTA{BT{bES {r*{R*{r* {B%s{B! -{R={C%d{R={B-{x" % (target.format_name_to(self), amount))
             self.act("{c[actor.name]'s punch {r*{R*{r* {bDE{BV{wASTA{BT{bES {r*{R*{r* {c[target.name]{c! {B-{R={C%d{R={B-{x" % (amount), target=target, exclude=[self, target])
             self.act_to(target, "{c[actor.name]'s punch {r*{R*{r* {bDE{BV{wASTA{BT{bES {r*{R*{r* {cyou! -{R={C%d{R={B-{x" % (amount))
+
             target.receive_damage(amount, self)
+
             target.save()
+            self.save()
 
     def receive_damage(self, amount, attacker):
         """Handle receiving damage."""
